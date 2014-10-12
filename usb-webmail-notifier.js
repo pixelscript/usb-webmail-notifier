c3c90db7f17c856335726f0fb31f5923a12a54db
Chris Corbett <chris@FRONT-END-DEV-MAC.local>
2014-10-12 22:39:29 +0100

    formatting

--- usb-webmail-notifier.js
+++ usb-webmail-notifier.js
@@ -1,43 +1,62 @@
 // Node.js USB "Webmail Notifier" based on a gist by mauricesvay //gist.github.com/mauricesvay/3920415
- 
+
 var HID = require('node-hid');
 var vendor = 4756;
 var product = 4896;
 var hid = new HID.HID(vendor, product);
 
-var Notifier = function(hid) {
+var Notifier = function (hid) {
+
     this.hid = hid;
     this.colors = ['OFF', 'GREEN', 'RED', 'BLUE', 'CYAN', 'YELLOW', 'PURPLE', 'WHITE'];
+
 };
-Notifier.prototype.write = function(arBytes) {
+
+Notifier.prototype.write = function (arBytes) {
+
     this.hid.write(arBytes);
+
 };
-Notifier.prototype.color = function(value) {
+
+Notifier.prototype.color = function (value) {
+
     var color;
+
     if (typeof value == 'number') {
         color = value;
     }
+
     if (typeof value == 'string') {
         color = this.colors.indexOf(value);
     }
-    this.write([ color, 0, 0, 0, 0, 255, 0, 0 ] );
+
+    this.write([ color, 0, 0, 0, 0, 255, 0, 0 ]);
+
     return this;
 };
-Notifier.prototype.off = function() {
+
+Notifier.prototype.off = function () {
+
     this.color(Notifier.OFF);
     return this;
+
 };
-Notifier.prototype.play = function(seq, delay, loop, i) {
+
+Notifier.prototype.play = function (seq, delay, loop, i) {
+
     var self = this;
     var color;
+
     if (typeof loop === 'undefined') {
         loop = false;
     }
+
     if (typeof i === 'undefined') {
         i = 0;
     }
- 
+
     color = seq[i];
+
     if (i < seq.length) {
         this.color(color);
         i++;
@@ -48,17 +67,19 @@ Notifier.prototype.play = function(seq, delay, loop, i) {
             return;
         }
     }
-    setTimeout(function(){
+
+    setTimeout(function () {
         self.play(seq, delay, loop, i);
     }, delay);
 };
 
-Notifier.prototype.flash = function(col,speed,times) {
-	if (typeof col === 'undefined') {
+Notifier.prototype.flash = function (col, speed, times) {
+
+    if (typeof col === 'undefined') {
         col = 'RED';
     }
 
-	if (typeof times === 'undefined') {
+    if (typeof times === 'undefined') {
         times = 50;
     }
 
@@ -66,19 +87,23 @@ Notifier.prototype.flash = function(col,speed,times) {
         speed = 10;
     }
 
-	var seq = [];
-	for (var i=0 ;i<times;i++) {
-		seq.push(col);
-		seq.push('OFF');
-	}
-	this.play(seq,speed);
+    var seq = [];
+
+    for (var i = 0; i < times; i++) {
+        seq.push(col);
+        seq.push('OFF');
+    }
+
+    this.play(seq, speed);
 }
 
-Notifier.prototype.cycle = function(cycle) {
-	var show = ['OFF', 'GREEN', 'RED', 'BLUE', 'CYAN', 'YELLOW', 'PURPLE', 'WHITE', 'OFF'];
-	this.play(show,1500);
+Notifier.prototype.cycle = function (cycle) {
+
+    var show = ['OFF', 'GREEN', 'RED', 'BLUE', 'CYAN', 'YELLOW', 'PURPLE', 'WHITE', 'OFF'];
+    this.play(show, 1500);
+
 }
- 
+
 // create instance of notifier with correct hid
 var notifier = new Notifier(hid);
 
